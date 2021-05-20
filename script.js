566ccff4851ca939d7979e5e35908bb174b67168
dstephenbenz <82403291+dstephenbenz@users.noreply.github.com>
2021-05-20 18:58:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,115 @@
-//console.log(document.getElementById("num").value);
+async function getToken() {
+    try {
+        const clientId = "c874b3e4bfb34f66aefdf74d3cb9ca17";
+        const clientSecret = "33f3f7df44cb4c11a06f303a447f27bc";
+        const result = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+            },
+            body: "grant_type=client_credentials",
 
+        });
+        const data = await result.json();
+        console.log(data);
+        const token = data.access_token;
+        getData(token);
 
-var a = document.getElementById("disp1");
-
-function handleClick(data) {
-    if (data == undefined) {
-        a.value = "";
-    } else {
-        a.value += data;
+    } catch (err) {
+        console.log(err)
     }
 
+}
+async function getData(token) {
 
+    try {
+        let resp = await fetch("https://api.spotify.com/v1/playlists/5jcwYGwEg9tvAxm9YbRrcw", {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + token,
+            }
+
+        });
+        const data = await resp.json();
+        console.log(data);
+        displayData(data);
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
-function clr() {
-    a.value = "";
-}
+function displayData(val) {
+    var x, y, z;
+    let img1 = val.images[0].url;
+    console.log(img1);
+    let name = val.name;
+    console.log(name);
+    //let trk = data.tracks['items'][0].track;
+    //console.log(trk);
+    for (var i = 0; i < 7; i++) {
+        if (val.tracks['items'][i].track !== null) {
+            x = val.tracks['items'][i].track.name;
+            y = val.tracks['items'][i].track.popularity;
+            z = val.tracks['items'][i].track.preview_url;
+            console.log(x, y, z);
+        }
 
-function solve() {
-    let s = a.value;
-    let w = eval(s);
-    a.value = w;
-}
-
-
-function add() {
-    a.value += "+";
-
-}
-
-
-function sub() {
-    a.value += "-";
-
-}
-
-
-function mul() {
-    a.value += "*";
+    }
+    document.getElementById('name').value = name;
+    document.getElementById('sngname').value = x;
+    document.getElementById('popular').value = y;
+    document.getElementById('prvtrack').value = z;
 
 }
+async function addTracks(token) {
+    try {
+        let resp = await fetch("https://api.spotify.com/v1/playlists/5jcwYGwEg9tvAxm9YbRrcw/tracks?uris=spotify%3Atrack%3A4iV5W9uYEdYUVa79Axb7Rh%2Cspotify%3Atrack%3A1301WleyT98MSxVHPZCA6M", {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer " + token,
+            }
 
-function div() {
-    a.value += "/";
+        });
+        const data = await resp.json();
+        console.log(data);
+
+    } catch (err) {
+        console.log(err);
+
+    }
+
+}
+async function deleteTracks(token) {
+    try {
+        let resp = await fetch("https://api.spotify.com/v1/playlists/5jcwYGwEg9tvAxm9YbRrcw", {
+            method: "DELETE",
+            headers: {
+                Authorization: "Bearer " + token,
+            }
+
+        });
+        const data = await resp.json();
+        console.log(data);
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+async function reorderTracks(token) {
+    try {
+        let resp = await fetch("https://api.spotify.com/v1/playlists/5jcwYGwEg9tvAxm9YbRrcw/tracks", {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + token,
+            }
+
+        });
+        const data = await resp.json();
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
 }
